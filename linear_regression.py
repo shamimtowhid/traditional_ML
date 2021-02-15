6b5460a23a8d3316dff639acbdaaddc9b55db789
shamimtowhid <shamim.towhid@gmail.com>
2021-02-15 20:53:49 +0900

    implemented predict function for regression models
    calculate prediction of two cases using the learned theta

--- linear_regression.py
+++ linear_regression.py
@@ -4,6 +4,7 @@ import numpy as np
 from plot_data import scatter_plot, plot, surface_plot, contour_plot
 from loss import mse_loss
 from gradient_descent import gd
+from utils import regression_predict
 
 if __name__=='__main__':
     #load data
@@ -24,25 +25,31 @@ if __name__=='__main__':
     alpha = 0.01
 
     history, theta = gd(X, y, theta, alpha, iterations)
-    #plot(history, xlabel='iterations', ylabel='loss values', title='training loss')
+    plot(history, xlabel='iterations', ylabel='loss values', title='training loss')
+
+    print("For population =35,000, we predict a profit of {}".format(
+            regression_predict(np.array([1, 3.5]), theta)*10000))
+
+    print("For population = 70,000, we predict a profit of {}".format(
+            regression_predict(np.array([1, 7.0]), theta)*10000))
 
     # visualize training data with linear regression fit
-    #plt = scatter_plot(data[0], data[1], show=False)
-    #plot(X[1],np.dot(X, theta), xlabel='population of city in 10,000s',
-    #                ylabel='profit in $10,000s', title='Training Data with regression fit')
+#    plt = scatter_plot(data[0], data[1], show=False)
+#    plot(X[1], regression_predict(X, theta), xlabel='population of city in 10,000s',
+#                    ylabel='profit in $10,000s', title='Training Data with regression fit')
 
     # visualize loss surface
-    theta0 = np.linspace(-10, 10, 100)
-    theta1 = np.linspace(-1, 4, 100)
+#    theta0 = np.linspace(-10, 10, 100)
+#    theta1 = np.linspace(-1, 4, 100)
 
-    losses = np.zeros((len(theta0), len(theta1)))
+#    losses = np.zeros((len(theta0), len(theta1)))
 
-    for i in range(len(theta0)):
-        for j in range(len(theta1)):
-            t = np.array([theta0[i], theta1[j]]).reshape(2,1)
-            losses[i][j] = mse_loss(X, y, t)
+#    for i in range(len(theta0)):
+#        for j in range(len(theta1)):
+#            t = np.array([theta0[i], theta1[j]]).reshape(2,1)
+#            losses[i][j] = mse_loss(X, y, t)
 
     #surface_plot(theta0, theta1, losses)
    
     # visualize contour plot
-    contour_plot(losses, levels=np.logspace(-2, 3, 20))
+    #contour_plot(losses, levels=np.logspace(-2, 3, 20))
---
+++ utils.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+def regression_predict(X, theta):
+    return np.dot(X, theta)
