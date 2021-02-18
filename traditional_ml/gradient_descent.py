246bba15f8aacb98e5ee716422ed88ee4462f9f1
shamimtowhid <shamim.towhid@gmail.com>
2021-02-18 21:09:16 +0900

    deleted unused gradient descent function

--- traditional_ml/gradient_descent.py
+++ traditional_ml/gradient_descent.py
@@ -17,22 +17,3 @@ def gd(X, y, theta, alpha, num_iter):
 #        print('Loss: ', l_hist[-1])
 
     return l_hist, theta
-
-
-def _gd(X, y, theta, alpha, num_iter):
-    l_hist = []
-    m = len(y)
-    for _ in range(num_iter):
-        hypothesis = (theta[:][1] * X[:][1]) + theta[:][0]
-        diff = (hypothesis - y)
-
-        gradient1 = np.sum(diff*X[:][0])
-        gradient2 = np.sum(diff*X[:][1])
-
-        theta[0][0] = theta[0][0] - alpha * ((1/m)*gradient1)
-        theta[1][0] = theta[1][0] - alpha * ((1/m)*gradient2)
-
-        l_hist.append(mse_loss(X, y, theta))
-#        print('Loss: ', l_hist[-1])
-
-    return l_hist, theta
