ab549b23a7000c409ecdf6ddc9a3008bfb5ec522
shamimtowhid <shamim.towhid@gmail.com>
2021-02-19 22:56:07 +0900

    added version information inside package

--- setup.py
+++ setup.py
@@ -1,11 +1,21 @@
+import re
 import setuptools
 
 with open("README.md", "r") as f:
     long_description = f.read()
 
+VERSIONFILE = "traditional_ml/version.py"
+verstrline = open(VERSIONFILE, "rt").read()
+VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
+mo = re.search(VSRE, verstrline, re.M)
+if mo:
+    verstr = mo.group(1)
+else:
+    raise RuntimeError("Unable to find version string in %s." % (VERSIONFILE,))
+
 setuptools.setup(
     name="traditional_ml",
-    version="0.0.0b",
+    version=verstr,
     author="Md. Shamim Towhid",
     author_email="shamim.towhid@gmail.com",
     description="A traditional machine learning library",
--- traditional_ml/__init__.py
+++ traditional_ml/__init__.py
@@ -1,3 +1,5 @@
+from traditional_ml.version import __version__
+
 from traditional_ml.gradient_descent import gd
 from traditional_ml.loss import mse_loss
 from traditional_ml.plot_data import scatter_plot, plot, surface_plot, contour_plot
