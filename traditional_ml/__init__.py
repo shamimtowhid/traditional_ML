79d83e15afbaac2a624185b047902f750110a376
shamimtowhid <shamim.towhid@gmail.com>
2021-02-19 21:40:29 +0900

    added flake8 changes

--- traditional_ml/__init__.py
+++ traditional_ml/__init__.py
@@ -1,4 +1,4 @@
-from traditional_ml.gradient_descent import *
-from traditional_ml.loss import *
-from traditional_ml.plot_data import *
-from traditional_ml.utils import *
+from traditional_ml.gradient_descent import gd
+from traditional_ml.loss import mse_loss
+from traditional_ml.plot_data import scatter_plot, plot, surface_plot, contour_plot
+from traditional_ml.utils import regression_predict, feature_normalize, initialize_theta_regression
