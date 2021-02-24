640d0128782e5b7c30fc6fd2746dd70b56b2dc89
shamimtowhid <shamim.towhid@gmail.com>
2021-02-24 22:00:41 +0900

    added flake8 changes

--- traditional_ml/logistic_regression/loss.py
+++ traditional_ml/logistic_regression/loss.py
@@ -6,23 +6,24 @@ from traditional_ml.logistic_regression.activation import sigmoid
 def gradient(X, y, theta):
     m, n = X.shape
 
-    y = y.reshape((m,1))
+    y = y.reshape((m, 1))
 
-    sigmoid_x_theta = sigmoid(x.dot(theta))
-    grad = ((x.T).dot(sigmoid_x_theta-y))/m
+    sigmoid_x_theta = sigmoid(X.dot(theta))
+    grad = ((X.T).dot(sigmoid_x_theta-y))/m
 
     return grad.flatten()
+
 
 def logistic_loss(X, y, theta):
     m, n = X.shape
 
-    y = y.reshape((m,1))
+    y = y.reshape((m, 1))
 
-    term1 = np.log(sigmoid(x.dot(theta)))
-    term2 = np.log(1-sigmoid(x.dot(theta)))
+    term1 = np.log(sigmoid(X.dot(theta)))
+    term2 = np.log(1-sigmoid(X.dot(theta)))
 
-    term1 = term1.reshape((m,1))
-    term2 = term2.reshape((m,1))
+    term1 = term1.reshape((m, 1))
+    term2 = term2.reshape((m, 1))
 
     term = y * term1 + (1 - y) * term2
 
