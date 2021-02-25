b67a132fe487c50588e2e7eefd13a586f467a647
shamimtowhid <shamim.towhid@gmail.com>
2021-02-25 21:56:34 +0900

    import logistic regression module in root package

--- traditional_ml/__init__.py
+++ traditional_ml/__init__.py
@@ -1,5 +1,6 @@
 from traditional_ml.version import __version__
 
 import traditional_ml.linear_regression
+import traditional_ml.logistic_regression
 from traditional_ml.plot_data import scatter_plot, plot, surface_plot, contour_plot
 from traditional_ml.utils import predict, feature_normalize, initialize_theta
