cadb55f22711cd451898cd9255c438e22b73b49a
shamimtowhid <shamim.towhid@gmail.com>
2021-02-25 21:55:27 +0900

    fix bug of logistic loss

--- traditional_ml/logistic_regression/loss.py
+++ traditional_ml/logistic_regression/loss.py
@@ -3,27 +3,29 @@ import numpy as np
 from traditional_ml.logistic_regression.activation import sigmoid
 
 
-def gradient(X, y, theta):
+def gradient(theta, X, y):
     m, n = X.shape
 
-    y = y.reshape((m, 1))
+    theta = theta.reshape((n,1))
+    y = y.values.reshape((m, 1))
 
     sigmoid_x_theta = sigmoid(X.dot(theta))
     grad = ((X.T).dot(sigmoid_x_theta-y))/m
 
-    return grad.flatten()
+    return grad.values.flatten()
 
 
-def logistic_loss(X, y, theta):
+def logistic_loss(theta, X, y):
     m, n = X.shape
 
-    y = y.reshape((m, 1))
+    theta = theta.reshape((n,1))
+    y = y.values.reshape((m, 1))
 
     term1 = np.log(sigmoid(X.dot(theta)))
     term2 = np.log(1-sigmoid(X.dot(theta)))
 
-    term1 = term1.reshape((m, 1))
-    term2 = term2.reshape((m, 1))
+    term1 = term1.values.reshape((m, 1))
+    term2 = term2.values.reshape((m, 1))
 
     term = y * term1 + (1 - y) * term2
 
