0fa61406f73a57caaa89f8d24d6919c8e15e9202
shamimtowhid <shamim.towhid@gmail.com>
2021-02-28 00:06:00 +0900

    implemented new optimize function for logistic regression
    implemented optimize function for multi class classification using logistic regression, also modify the predict function og logistic regression to suppoer multi class prediction

--- traditional_ml/logistic_regression/optimize.py
+++ traditional_ml/logistic_regression/optimize.py
@@ -1,4 +1,7 @@
+import numpy as np
 import scipy.optimize as op
+
+from traditional_ml.utils import initialize_theta
 
 
 def optimize_theta(X, y, loss_fn, theta, gradient_fn, lamda=0):
@@ -8,3 +11,23 @@ def optimize_theta(X, y, loss_fn, theta, gradient_fn, lamda=0):
                          method='TNC',
                          jac=gradient_fn)
     return result.x, result.fun
+
+
+def optimize_theta_for_multiclass(X, y, loss_fn, gradient_fn, cls_idx, lamda=0.1):
+    num_feature = X.shape[1]
+    num_class = len(cls_idx)
+    theta = np.zeros((num_class, num_feature))  # +1 is for the inception in feature vector
+
+    loss = []
+    for i, cls in enumerate(cls_idx):
+        lbl = np.array(y == cls).astype(int)
+        init_theta = initialize_theta(num_feature)
+        result = op.minimize(fun=loss_fn,
+                             x0=init_theta,
+                             args=(X, lbl, lamda),
+                             method='TNC',
+                             jac=gradient_fn)
+        loss.append(result.fun)
+        theta[i] = result.x
+
+    return theta, loss
--- traditional_ml/utils.py
+++ traditional_ml/utils.py
@@ -27,16 +27,18 @@ def initialize_theta(num_features):
     return np.zeros((num_features, 1))
 
 
-def log_regression_predict(X, theta):
+def log_regression_predict(X, theta, num_cls=2):
     m, n = X.shape
     p = np.zeros((m, 1))
 
     h = sigmoid(X.dot(theta.T))
 
-    for it in range(0, h.shape[0]):
-        if h[it] > 0.5:
-            p[it, 0] = 1
-        else:
-            p[it, 0] = 0
-
-    return p
+    if num_cls > 2:
+        return h  # returns the probability of each class
+    else:
+        for it in range(0, h.shape[0]):
+            if h[it] > 0.5:
+                p[it, 0] = 1
+            else:
+                p[it, 0] = 0
+        return p
