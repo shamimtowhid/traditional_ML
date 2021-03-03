9f289ce092ab57645ee06988c6c5d64aed22f368
shamimtowhid <shamim.towhid@gmail.com>
2021-03-03 22:57:38 +0900

    modify nn_predict function to be efficient than before

--- traditional_ml/neural_network/predict.py
+++ traditional_ml/neural_network/predict.py
@@ -3,9 +3,12 @@ import numpy as np
 from traditional_ml.logistic_regression import sigmoid
 
 
-def nn_predict(X: np.ndarray, theta_list: list) -> tuple:
-    layer_out = X
-    for i, theta in enumerate(theta_list):
-        layer_out = sigmoid(np.insert(layer_out, 0, 1, axis=1).dot(theta.T))
-
-    return np.argmax(layer_out), layer_out
+def nn_predict(Theta1, Theta2, X):
+    m= X.shape[0]
+    X = np.hstack((np.ones((m,1)),X))
+    
+    a1 = sigmoid(X @ Theta1.T)
+    a1 = np.hstack((np.ones((m,1)), a1)) # hidden layer
+    a2 = sigmoid(a1 @ Theta2.T) # output layer
+    
+    return np.argmax(a2,axis=1)+1
