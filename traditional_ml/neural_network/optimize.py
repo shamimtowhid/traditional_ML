4acee66c854ba0833a7f7ef3414412ceb64556bc
shamimtowhid <shamim.towhid@gmail.com>
2021-03-04 00:26:40 +0900

    implemented optimization function for neural network

--- traditional_ml/neural_network/optimize.py
+++ traditional_ml/neural_network/optimize.py
@@ -1,13 +1,22 @@
-import scipy.optimize as op
+import numpy as np
+from tqdm import tqdm
 
 from traditional_ml.neural_network.loss import nn_loss
 
 
-def optimize_theta(nn_params, input_layer_size, hidden_layer_size, num_labels, X, y, lamda):
-    theta = (nn_params.T).ravel()
-    result = op.minimize(fun=nn_loss, x0=theta, args=(nn_params,
-                                     input_layer_size, hidden_layer_size, num_labels,
-                                     X, y, lamda), method='CG', jac=nn_loss_grad
-                    )
+def optimize_theta(X, y, initial_nn_params, alpha, num_iters, Lambda, input_layer_size, hidden_layer_size, num_labels):
+    Theta1 = initial_nn_params[:((input_layer_size+1) * hidden_layer_size)
+                               ].reshape(hidden_layer_size, input_layer_size+1)
+    Theta2 = initial_nn_params[((input_layer_size + 1) * hidden_layer_size):].reshape(num_labels, hidden_layer_size+1)
 
-    return result.x, result.fun
+    J_history = []
+
+    for i in tqdm(range(num_iters)):
+        nn_params = np.append(Theta1.flatten(), Theta2.flatten())
+        cost, grad1, grad2 = nn_loss(nn_params, input_layer_size, hidden_layer_size, num_labels, X, y, Lambda)[3:]
+        Theta1 = Theta1 - (alpha * grad1)
+        Theta2 = Theta2 - (alpha * grad2)
+        J_history.append(cost)
+
+    nn_paramsFinal = np.append(Theta1.flatten(), Theta2.flatten())
+    return nn_paramsFinal, J_history
--- traditional_ml/neural_network/predict.py
+++ traditional_ml/neural_network/predict.py
@@ -4,11 +4,11 @@ from traditional_ml.logistic_regression import sigmoid
 
 
 def nn_predict(Theta1, Theta2, X):
-    m= X.shape[0]
-    X = np.hstack((np.ones((m,1)),X))
-    
+    m = X.shape[0]
+    X = np.hstack((np.ones((m, 1)), X))
+
     a1 = sigmoid(X @ Theta1.T)
-    a1 = np.hstack((np.ones((m,1)), a1)) # hidden layer
-    a2 = sigmoid(a1 @ Theta2.T) # output layer
-    
-    return np.argmax(a2,axis=1)+1
+    a1 = np.hstack((np.ones((m, 1)), a1))  # hidden layer
+    a2 = sigmoid(a1 @ Theta2.T)  # output layer
+
+    return np.argmax(a2, axis=1)+1
