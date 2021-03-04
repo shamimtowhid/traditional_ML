2f1c083544c493977d33bdeb2aa76781ecd9db0c
shamimtowhid <shamim.towhid@gmail.com>
2021-03-04 22:13:14 +0900

    add meaningful variable name to the optimize function of neural network

--- traditional_ml/neural_network/optimize.py
+++ traditional_ml/neural_network/optimize.py
@@ -4,19 +4,19 @@ from tqdm import tqdm
 from traditional_ml.neural_network.loss import nn_loss
 
 
-def optimize_theta(X, y, initial_nn_params, alpha, num_iters, Lambda, input_layer_size, hidden_layer_size, num_labels):
-    Theta1 = initial_nn_params[:((input_layer_size+1) * hidden_layer_size)
-                               ].reshape(hidden_layer_size, input_layer_size+1)
-    Theta2 = initial_nn_params[((input_layer_size + 1) * hidden_layer_size):].reshape(num_labels, hidden_layer_size+1)
+def optimize_theta(X, y, theta, lr, iterations, Lambda, input_layer_size, hidden_layer_size, num_labels):
+    theta1 = theta[:((input_layer_size+1) * hidden_layer_size)
+                   ].reshape(hidden_layer_size, input_layer_size+1)
+    theta2 = theta[((input_layer_size + 1) * hidden_layer_size):].reshape(num_labels, hidden_layer_size+1)
 
-    J_history = []
+    loss_history = []
 
-    for i in tqdm(range(num_iters)):
-        nn_params = np.append(Theta1.flatten(), Theta2.flatten())
-        cost, grad1, grad2 = nn_loss(nn_params, input_layer_size, hidden_layer_size, num_labels, X, y, Lambda)[3:]
-        Theta1 = Theta1 - (alpha * grad1)
-        Theta2 = Theta2 - (alpha * grad2)
-        J_history.append(cost)
+    for i in tqdm(range(iterations)):
+        nn_params = np.append(theta1.flatten(), theta2.flatten())
+        loss, grad1, grad2 = nn_loss(nn_params, input_layer_size, hidden_layer_size, num_labels, X, y, Lambda)[3:]
+        theta1 = theta1 - (lr * grad1)
+        theta2 = theta2 - (lr * grad2)
+        loss_history.append(loss)
 
-    nn_paramsFinal = np.append(Theta1.flatten(), Theta2.flatten())
-    return nn_paramsFinal, J_history
+    final_nn_params = np.append(theta1.flatten(), theta2.flatten())
+    return final_nn_params, loss_history
