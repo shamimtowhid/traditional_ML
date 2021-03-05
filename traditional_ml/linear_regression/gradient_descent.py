3b6f9c7ae5c03107cafa6a8dd634d37907b311fb
shamimtowhid <shamim.towhid@gmail.com>
2021-03-05 23:50:37 +0900

    implemented gradient descent for linear regression

--- traditional_ml/linear_regression/gradient_descent.py
+++ traditional_ml/linear_regression/gradient_descent.py
@@ -1,19 +1,34 @@
 import numpy as np
+from tqdm import tqdm
 
-from traditional_ml.linear_regression.loss import mse_loss
+from traditional_ml.linear_regression.loss import mse_loss, mse_loss_gradient
 
 
-def gd(X, y, theta, alpha, num_iter):
+#def gd(X, y, theta, alpha, num_iter):
+#    l_hist = []
+#    m = len(y)
+#    for _ in range(num_iter):
+#        hypothesis = np.dot(X, theta)
+#        diff = (hypothesis - y.values.reshape(-1, 1))
+#
+#        gradients = np.sum(diff*X, axis=0).values.reshape(-1, 1)
+#        theta = theta - alpha * ((1/m)*gradients)
+#
+#        l_hist.append(mse_loss(X, y, theta))
+#        print('Loss: ', l_hist[-1])
+#
+#    return l_hist, theta
+
+
+def gd(X, y, theta, alpha, num_iter, lamda=0):
     l_hist = []
     m = len(y)
-    for _ in range(num_iter):
-        hypothesis = np.dot(X, theta)
-        diff = (hypothesis - y.values.reshape(-1, 1))
+    y = y.values if hasattr(y, 'values') else y
 
-        gradients = np.sum(diff*X, axis=0).values.reshape(-1, 1)
-        theta = theta - alpha * ((1/m)*gradients)
+    for _ in tqdm(range(num_iter)):
+        l_hist.append(mse_loss(X, y, theta, lamda))
 
-        l_hist.append(mse_loss(X, y, theta))
-#        print('Loss: ', l_hist[-1])
+        gradients = mse_loss_gradient(X, y, theta, lamda)
+        theta = theta - (alpha * gradients)
 
     return l_hist, theta
